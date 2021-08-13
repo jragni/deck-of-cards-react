dd26e4cf2a7eee71fcb92ecd4e40de636d0c7910
Ray Agni <jagni001@ucr.edu>
2021-08-13 15:58:09 -0700

    FEAT: completed functioning MVP prior to code review

--- src/ShuffleApp.js
+++ src/ShuffleApp.js
@@ -1,48 +1,55 @@
-import axios from 'axios';
-import React, {useState, useEffect} from 'react';
-import DrawnCards from './DrawnCards';
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import DrawnCards from "./DrawnCards";
 
 /** ShuffleApp
- * 
+ *
  * Shuffles a new a deck and handles drawing of cards from deck
- * 
+ *
  * States:
  * - drawnCards [{card}, {card}, ...]
  *      where card is like { image, images, code, suit, value }
  * - deck { success, deck_id, shuffled, remaining }
  * - isShuffling boolean whether deck needs be shuffled or not
- * 
+ *
  */
 function ShuffleApp() {
-    const [drawnCards, setDrawnCards] = useState([]);
-    const [deck, setDeck] = useState({});
-    const [isShuffling, setShuffling] = useState(true);
+  const [drawnCards, setDrawnCards] = useState([]);
+  const [deck, setDeck] = useState({});
+  const [isShuffling, setShuffling] = useState(true);
 
-
-    useEffect(function shuffleDeckWhenMounted() {
-        async function shuffle() {
-            const resp = await axios.get('http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-            setDeck(resp.data);
-            setShuffling(false);
-        }
-        shuffle();
-    }, [ ]);
-
-    async function handleDraw() {
-        const resp = await axios.get(`http://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`);
-        const card = resp.data.cards[0];
-        setDrawnCards(drawnCards => [...drawnCards, card]);
+  useEffect(function shuffleDeckWhenMounted() {
+    /** gets shuffled deck from api*/
+    async function shuffle() {
+      const resp = await axios.get(
+        "http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+      );
+      setDeck(resp.data);
+      setShuffling(false);
     }
+    shuffle();
+  }, []);
 
-    if (isShuffling) return <i>Shuffling Deck</i>
+  /** draws card from deck api and updates the drawn cards */
+  async function handleDraw() {
+    const resp = await axios.get(
+      `http://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`
+    );
+    const card = resp.data.cards[0];
+    if (card) setDrawnCards((drawnCards) => [...drawnCards, card]);
+  }
 
-    return (
-        <div className="ShuffleApp">
-            <button onClick={handleDraw}>Draw a card!</button>
-            <DrawnCards cards={drawnCards}/>
-        </div>
-    )
+  if (isShuffling) return <i>Shuffling Deck</i>;
+
+  return (
+    <div className="ShuffleApp">
+      {drawnCards.length === 52 
+        ? (<i> {alert('No Cards Remaining!')}</i>) 
+        : ( <button onClick={handleDraw}>Draw a card!</button>)
+      }
+      <DrawnCards cards={drawnCards} />
+    </div>
+  );
 }
 
 export default ShuffleApp;
